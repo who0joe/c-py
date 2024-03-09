0fe25bf372aa1d0cc17174b6ae23470b46937fe4
GIT_USER_NAME <GIT_USER_EMAIL>
2024-03-09 20:29:39 +0900

    update

---
+++ .devcontainer/config.bash
@@ -0,0 +1,22 @@
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -alF'
+alias lo='ls -ort'
+alias la='ls -A'
+alias l='ls -CF'
+alias lsv='ls -l --time-style='+%Y.%m.%d''
+alias llsort='ls -al | sort +8'
+alias cls='clear'
+alias vi="vim"
